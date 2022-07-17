d8691e3addc3ac84069deef576df92cdf7068a78
Ameya Subhash <84078540+ameyasubhash@users.noreply.github.com>
2022-07-17 15:26:25 +0530

    uploading dice game code files

---
+++ dice.js
@@ -0,0 +1,15 @@
+var randomNum1=Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src", "images/dice"+randomNum1+".png");
+
+var randomNum2=Math.floor(Math.random()*6)+1;
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomNum2+".png");
+
+if(randomNum1==randomNum2){
+    document.querySelector("h1").innerHTML="Draw !";
+}
+else if(randomNum1>randomNum2){
+    document.querySelector("h1").innerHTML="Player 1 win! 🚩";
+}
+else{
+    document.querySelector("h1").innerHTML="Player 2 win! 🚩";
+}
